6474b32f6c3f5b03f633b3a7e82463975c1b4e24
Gayathri835 <banothgayathri08@gmail.com>
2026-04-03 15:36:20 +0530

    Update classifier.py

--- backend/services/classifier.py
+++ backend/services/classifier.py
@@ -1,1 +1,11 @@
+def classify_intent(text: str):
+    text = text.lower()
 
+    if "suicide" in text or "die" in text:
+        return "crisis"
+    elif "sad" in text or "depressed" in text:
+        return "depression"
+    elif "anxious" in text or "nervous" in text:
+        return "anxiety"
+    else:
+        return "normal"
